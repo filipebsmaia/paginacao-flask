420d19f5bd2439ee8805e894558650ee58216e5f
Filipe Maia <filipebsmaia@gmail.com>
2022-05-30 19:17:14 -0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from persons import persons
+
+print(persons)
+
+app = Flask(__name__)
+
+PAGE_LENGTH = 12
+
+def toPaginatedList(list):
+    return [list[i:i+PAGE_LENGTH] for i in range(0, len(list), PAGE_LENGTH)] 
+
+
+@app.route("/")
+def index():
+    page = request.args.get('page', 1, type=int)
+    list = toPaginatedList(persons)
+
+    if page < 1:
+        page = 1
+    if page > len(list):
+        page = len(list)
+
+    return render_template('index.html', persons={
+        "list": list[page - 1],
+        "pages": len(list),
+        "page": page
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ persons.py
@@ -0,0 +1,242 @@
+persons = [
+  {
+    "nome": "Pietra",
+    "surname": "Rosa Isabelle Moreira"
+  },
+  {
+    "nome": "Lavínia",
+    "surname": "Allana Duarte"
+  },
+  {
+    "nome": "Lívia",
+    "surname": "Isadora Gabrielly da Cunha"
+  },
+  {
+    "nome": "Thiago",
+    "surname": "Francisco Isaac Costa"
+  },
+  {
+    "nome": "Milena",
+    "surname": "Fabiana Castro"
+  },
+  {
+    "nome": "Sebastião",
+    "surname": "Bento Pinto"
+  },
+  {
+    "nome": "Cláudio",
+    "surname": "Nathan Igor Pereira"
+  },
+  {
+    "nome": "Gael",
+    "surname": "Pedro Davi Gonçalves"
+  },
+  {
+    "nome": "Mariah",
+    "surname": "Vanessa da Paz"
+  },
+  {
+    "nome": "Bárbara",
+    "surname": "Clara Figueiredo"
+  },
+  {
+    "nome": "Sebastiana",
+    "surname": "Kamilly Corte Real"
+  },
+  {
+    "nome": "Cauê",
+    "surname": "Tomás Renan Lima"
+  },
+  {
+    "nome": "Lucca",
+    "surname": "Lucas Araújo"
+  },
+  {
+    "nome": "Marlene",
+    "surname": "Valentina Almada"
+  },
+  {
+    "nome": "Rita",
+    "surname": "Vitória Ribeiro"
+  },
+  {
+    "nome": "Amanda",
+    "surname": "Isis Larissa Silveira"
+  },
+  {
+    "nome": "Elza",
+    "surname": "Fátima Marlene Rocha"
+  },
+  {
+    "nome": "Manuel",
+    "surname": "Lucas Manuel Fernandes"
+  },
+  {
+    "nome": "Sandra",
+    "surname": "Rosa Martins"
+  },
+  {
+    "nome": "Luiza",
+    "surname": "Isis Porto"
+  },
+  {
+    "nome": "Stella",
+    "surname": "Joana Letícia Nogueira"
+  },
+  {
+    "nome": "Alessandra",
+    "surname": "Clarice Letícia Aragão"
+  },
+  {
+    "nome": "Sebastião",
+    "surname": "Leandro Brito"
+  },
+  {
+    "nome": "Brenda",
+    "surname": "Eloá Vitória Cardoso"
+  },
+  {
+    "nome": "Caio",
+    "surname": "Osvaldo Assis"
+  },
+  {
+    "nome": "Antonio",
+    "surname": "Heitor Alves"
+  },
+  {
+    "nome": "José",
+    "surname": "Theo Marcos Almeida"
+  },
+  {
+    "nome": "Manuela",
+    "surname": "Clarice Sônia Barbosa"
+  },
+  {
+    "nome": "Rafaela",
+    "surname": "Alana Gomes"
+  },
+  {
+    "nome": "Cláudio",
+    "surname": "Leonardo Giovanni da Silva"
+  },
+  {
+    "nome": "Melissa",
+    "surname": "Nina Peixoto"
+  },
+  {
+    "nome": "Mariana",
+    "surname": "Elza Nascimento"
+  },
+  {
+    "nome": "Benício",
+    "surname": "Julio Ryan Galvão"
+  },
+  {
+    "nome": "Sônia",
+    "surname": "Isabelle Aragão"
+  },
+  {
+    "nome": "Sebastião",
+    "surname": "Hugo Manuel Carvalho"
+  },
+  {
+    "nome": "Josefa",
+    "surname": "Jéssica Evelyn Baptista"
+  },
+  {
+    "nome": "Alana",
+    "surname": "Cecília Alana Moraes"
+  },
+  {
+    "nome": "Bruno",
+    "surname": "Yuri Moreira"
+  },
+  {
+    "nome": "Tânia",
+    "surname": "Teresinha Isadora Nogueira"
+  },
+  {
+    "nome": "Emily",
+    "surname": "Isabel Jaqueline Pereira"
+  },
+  {
+    "nome": "Cauê",
+    "surname": "Ricardo Ricardo Viana"
+  },
+  {
+    "nome": "Analu",
+    "surname": "Elaine Mariana Barbosa"
+  },
+  {
+    "nome": "Enzo",
+    "surname": "Renato da Luz"
+  },
+  {
+    "nome": "Cauê",
+    "surname": "Eduardo Pietro Costa"
+  },
+  {
+    "nome": "Mateus",
+    "surname": "Henrique Pedro Henrique Dias"
+  },
+  {
+    "nome": "Emanuelly",
+    "surname": "Sandra Mariah Assis"
+  },
+  {
+    "nome": "Natália",
+    "surname": "Isis Louise Assunção"
+  },
+  {
+    "nome": "Nelson",
+    "surname": "Bernardo Silveira"
+  },
+  {
+    "nome": "Manuela",
+    "surname": "Eliane Fernandes"
+  },
+  {
+    "nome": "Jorge",
+    "surname": "Diogo Anderson da Silva"
+  },
+  {
+    "nome": "Sebastião",
+    "surname": "Emanuel Monteiro"
+  },
+  {
+    "nome": "Murilo",
+    "surname": "Antonio Ribeiro"
+  },
+  {
+    "nome": "Carolina",
+    "surname": "Sebastiana Alice da Mata"
+  },
+  {
+    "nome": "Giovana",
+    "surname": "Nair Lara Cavalcanti"
+  },
+  {
+    "nome": "Isabelle",
+    "surname": "Isabel Renata Aparício"
+  },
+  {
+    "nome": "Rosângela",
+    "surname": "Isabela Aurora Rodrigues"
+  },
+  {
+    "nome": "Isis",
+    "surname": "Carla Ferreira"
+  },
+  {
+    "nome": "Isabel",
+    "surname": "Eduarda Pinto"
+  },
+  {
+    "nome": "Gabriela",
+    "surname": "Rayssa Sophia Sales"
+  },
+  {
+    "nome": "Leonardo",
+    "surname": "Roberto Diogo Araújo"
+  }
+]
